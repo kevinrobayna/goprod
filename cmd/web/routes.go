dfc127265adee6d35b5b36cd57c337eeed640f25
Kevin Robayna <me@kevinrobayna.com>
2022-08-12 10:32:26 +0100

    Use `github.com/kevinrobayna/goprod` as module
    This is the standard way in go, so let's follow it. It's also the same in java to avoid package clash since no one should be working on the same package as you.

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/kevinrobayna/goprod/internal/models"
 	"go.uber.org/zap"
-	"goprod/internal/models"
 	"gorm.io/gorm"
 	"net/http"
 )
