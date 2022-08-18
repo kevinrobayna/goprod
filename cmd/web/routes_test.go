4c51d1b678c5e1b53e93afeb364de2cb0866fbad
Kevin Robayna <me@kevinrobayna.com>
2022-08-18 09:05:05 +0100

    Use Testcontainers to spin up postgres (#10)
    In this PR I change the usage of SQLite for postgres. The only reason is that while I was working on #7, the DB is stored on disk so it was annoying for my integration tests.
    
    
    TestContainers is a fantastic tool that spins ups docker containers for you without much issue. This allows us to test without having a service running outside of the tests making sure that we truncate or empty every table we used. It's just nice! I've always used it in java and the GoLang version is a lot more descriptive but everything in GoLang is.
    
    While I was working on this I wanted to learn what's the format for the DB connection which gorm.io calls DSN. This is a great post about it, https://stackoverflow.com/questions/60465376/is-there-a-standard-dsn-format
    
    One issue that occurred as part of this change is that the e2e tests do not like to run in parallel, probably because of the use of port 8080 for the web app. I need to change this soon so that my tests run quick! Raising https://github.com/kevinrobayna/goprod/issues/11 for this

--- cmd/web/routes_test.go
+++ cmd/web/routes_test.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"github.com/kevinrobayna/goprod/internal/di"
 	"github.com/kevinrobayna/goprod/internal/models"
 	"github.com/stretchr/testify/assert"
+	"github.com/testcontainers/testcontainers-go"
+	"go.uber.org/fx"
 	"go.uber.org/fx/fxtest"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"testing"
 )
@@ -15,60 +19,63 @@ type Message struct {
 }
 
 func TestRoutes(t *testing.T) {
-	t.Parallel()
-
 	t.Run("Hello", func(t *testing.T) {
-		t.Parallel()
-		app := fxtest.New(t, opts())
-		// Starts the app right away, and defers a stop when the test ends.
+		ctx := context.Background()
+
+		pg, err := di.TestWithPostgres(ctx)
+		assert.NoError(t, err)
+		defer func(postgresC testcontainers.Container, ctx context.Context) {
+			err := pg.Container.Terminate(ctx)
+			if err != nil {
+				t.Error(err)
+			}
+		}(pg.Container, ctx)
+
+		app := fxtest.New(t, opts(), fx.Replace(pg.Config))
 		defer app.RequireStart().RequireStop()
 
 		resp, err := http.Get("http://localhost:8080/")
-		if err != nil {
-			panic(err.Error())
-		}
+		assert.NoError(t, err)
 
 		assert.Equal(t, http.StatusOK, resp.StatusCode)
 
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err.Error())
-		}
+		body, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
 
 		var message = Message{}
 		err = json.Unmarshal(body, &message)
-		if err != nil {
-			panic(err.Error())
-		}
+		assert.NoError(t, err)
 
 		assert.Equal(t, "hello", message.Message)
 	})
 
 	t.Run("Ping", func(t *testing.T) {
-		t.Parallel()
+		ctx := context.Background()
+		data, err := di.TestWithPostgres(ctx)
+		assert.NoError(t, err)
+		defer func(postgresC testcontainers.Container, ctx context.Context) {
+			err := data.Container.Terminate(ctx)
+			if err != nil {
+				t.Error(err)
+			}
+		}(data.Container, ctx)
 
-		app := fxtest.New(t, opts())
-		// Starts the app right away, and defers a stop when the test ends.
+		app := fxtest.New(t, opts(), fx.Replace(data.Config))
 		defer app.RequireStart().RequireStop()
 
 		resp, err := http.Get("http://localhost:8080/ping")
-		if err != nil {
-			panic(err.Error())
-		}
+		assert.NoError(t, err)
 
 		assert.Equal(t, http.StatusOK, resp.StatusCode)
 
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			panic(err.Error())
-		}
+		body, err := io.ReadAll(resp.Body)
+		assert.NoError(t, err)
 
 		var product = models.Product{}
 		err = json.Unmarshal(body, &product)
-		if err != nil {
-			panic(err.Error())
-		}
+		assert.NoError(t, err)
 
+		assert.NotEmpty(t, product)
 		assert.Equal(t, "D42", product.Code)
 		assert.Equal(t, uint(100), product.Price)
 	})
---
+++ internal/di/test_utils.go
@@ -0,0 +1,52 @@
+package di
+
+import (
+	"context"
+	"github.com/testcontainers/testcontainers-go"
+	"github.com/testcontainers/testcontainers-go/wait"
+)
+
+type DBContainer struct {
+	Container testcontainers.Container
+	Config    DBConfig
+}
+
+func TestWithPostgres(ctx context.Context) (DBContainer, error) {
+	req := testcontainers.ContainerRequest{
+		Image: "postgres:14.5",
+		Env: map[string]string{
+			"POSTGRES_USER":             "goprod",
+			"POSTGRES_PASSWORD":         "",
+			"POSTGRES_DB":               "goprod",
+			"POSTGRES_HOST_AUTH_METHOD": "trust",
+		},
+		ExposedPorts: []string{"5432/tcp"},
+		WaitingFor:   wait.ForListeningPort("5432/tcp"),
+	}
+	postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
+		ContainerRequest: req,
+		Started:          true,
+	})
+	if err != nil {
+		return DBContainer{}, err
+	}
+	port, err := postgresC.MappedPort(ctx, "5432/tcp")
+	if err != nil {
+		return DBContainer{}, err
+	}
+	return DBContainer{
+		Container: postgresC,
+		Config:    config(port.Port()),
+	}, nil
+}
+
+func config(port string) DBConfig {
+	return DBConfig{
+		Host:     "localhost",
+		Port:     port,
+		User:     "goprod",
+		Password: "",
+		DbName:   "goprod",
+		SslMode:  "disable",
+	}
+}
