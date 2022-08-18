848294591f1d8d0a1697af74cce3f38ba86ef95e
Kevin Robayna <me@kevinrobayna.com>
2022-08-18 08:45:22 +0100

    refactor test utils

--- cmd/web/routes_test.go
+++ cmd/web/routes_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/kevinrobayna/goprod/internal/models"
 	"github.com/stretchr/testify/assert"
 	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/wait"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxtest"
 	"io"
@@ -19,50 +18,20 @@ type Message struct {
 	Message string `json:"message"`
 }
 
-func SetupWithPostgres() testcontainers.ContainerRequest {
-	return testcontainers.ContainerRequest{
-		Image: "postgres:14.5",
-		Env: map[string]string{
-			"POSTGRES_USER":             "goprod",
-			"POSTGRES_PASSWORD":         "",
-			"POSTGRES_DB":               "goprod",
-			"POSTGRES_HOST_AUTH_METHOD": "trust",
-		},
-		ExposedPorts: []string{"5432/tcp"},
-		WaitingFor:   wait.ForListeningPort("5432/tcp"),
-	}
-}
-
-func config(port string) di.DBConfig {
-	return di.DBConfig{
-		Host:     "localhost",
-		Port:     port,
-		User:     "goprod",
-		Password: "",
-		DbName:   "goprod",
-		SslMode:  "disable",
-	}
-}
-
 func TestRoutes(t *testing.T) {
 	t.Run("Hello", func(t *testing.T) {
 		ctx := context.Background()
-		req := SetupWithPostgres()
-		postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-			ContainerRequest: req,
-			Started:          true,
-		})
+
+		data, err := di.TestWithPostgres(ctx)
 		assert.NoError(t, err)
 		defer func(postgresC testcontainers.Container, ctx context.Context) {
-			err := postgresC.Terminate(ctx)
+			err := data.Container.Terminate(ctx)
 			if err != nil {
 				t.Error(err)
 			}
-		}(postgresC, ctx)
+		}(data.Container, ctx)
 
-		port, err := postgresC.MappedPort(ctx, "5432/tcp")
-		assert.NoError(t, err)
-		app := fxtest.New(t, opts(), fx.Replace(config(port.Port())))
+		app := fxtest.New(t, opts(), fx.Replace(data.Config))
 		defer app.RequireStart().RequireStop()
 
 		resp, err := http.Get("http://localhost:8080/")
@@ -82,22 +51,16 @@ func TestRoutes(t *testing.T) {
 
 	t.Run("Ping", func(t *testing.T) {
 		ctx := context.Background()
-		req := SetupWithPostgres()
-		postgresC, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
-			ContainerRequest: req,
-			Started:          true,
-		})
+		data, err := di.TestWithPostgres(ctx)
 		assert.NoError(t, err)
 		defer func(postgresC testcontainers.Container, ctx context.Context) {
-			err := postgresC.Terminate(ctx)
+			err := data.Container.Terminate(ctx)
 			if err != nil {
 				t.Error(err)
 			}
-		}(postgresC, ctx)
+		}(data.Container, ctx)
 
-		port, err := postgresC.MappedPort(ctx, "5432/tcp")
-		assert.NoError(t, err)
-		app := fxtest.New(t, opts(), fx.Replace(config(port.Port())))
+		app := fxtest.New(t, opts(), fx.Replace(data.Config))
 		defer app.RequireStart().RequireStop()
 
 		resp, err := http.Get("http://localhost:8080/ping")
